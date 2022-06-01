bb1ea9f07549a8d805a72141877609aab7cac6d8
Joyce Bastos <joyce.bastos-ext@viavarejo.com.br>
2022-06-01 17:12:53 -0300

    atualizacao do cenario de testes

--- cypress/integration/LoginTest.spec.js
+++ cypress/integration/LoginTest.spec.js
@@ -5,8 +5,8 @@ let email = Math.random().toString(36).slice(2, 7)+"@mailinator.com";
         it('Login com sucesso', ()=> {
             cy.visit('https://inscricoes.agilebrazil.com/users/sign_in');//Abre URL
             cy.get('[href="/users/sign_up"]').click();//clicar no link registro
-            cy.get('#user_first_name').type('Joyce');//Inserir o nome
-            cy.get('#user_last_name').type('Bastos');//Inserir o sobrenome
+            cy.get('#user_first_name').type('Testes');//Inserir o nome
+            cy.get('#user_last_name').type('Agile');//Inserir o sobrenome
             cy.get('#user_email').type(email)//Inserir email
             cy.get('#user_password').type('12345678')//Inserir a senha
             cy.get('#user_password_confirmation').type('12345678')//Inserir a confirmação da senha
